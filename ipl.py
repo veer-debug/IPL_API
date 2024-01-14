a1112a7cd5ac623adb822f0c3cbf0072c773205b
Ranveer <theveer07ranveer@gmail.com>
2024-01-15 01:19:27 +0530

    -

--- ipl.py
+++ ipl.py
@@ -4,7 +4,7 @@ import numpy as np
 ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRy2DUdUbaKx_Co9F0FSnIlyS-8kp4aKv_I0-qzNeghiZHAI_hw94gKG22XTxNJHMFnFVKsO4xWOdIs/pub?gid=1655759976&single=true&output=csv"
 matches = pd.read_csv(ipl_matches)
 
-print(matches.head())
+# print(matches.head())
 
 def teamsAPI():
     teams = list(set(list(matches['Team1']) + list(matches['Team2'])))
